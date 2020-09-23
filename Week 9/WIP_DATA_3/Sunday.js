d92d56fd433f7b7e8eb9d0dcc97e0f814144a5d3
aliceyu1111 <68723247+aliceyu1111@users.noreply.github.com>
2020-09-23 11:31:37 +1000

    Add files via upload

---
+++ Week 9/WIP_DATA_3/Sunday.js
@@ -0,0 +1,267 @@
+//Notifications received on the 16/08/20
+ [
+    {
+          "12:00 am": {
+        "instagram": 0,
+        "messenger": 0,
+        "snapchat": 0,
+        "email": 0,
+        "facebook": 0,
+        "totalNotification": 0
+          }
+    },
+
+    {
+         "1:00 am": {
+        "instagram": 1,
+        "messenger": 2,
+        "snapchat": 0,
+        "email": 0,
+        "facebook": 0,
+        "totalNotification": 3
+         }
+    },
+    
+    
+    {
+         "2:00 am": {
+        "instagram": 0,
+        "messenger": 0,
+        "snapchat": 0,
+        "email": 0,
+        "facebook": 0,
+        "totalNotification": 0
+         }
+    },
+    
+    {
+         "3:00 am": {
+        "instagram": 0,
+        "messenger": 0,
+        "snapchat": 0,
+        "email": 0,
+        "facebook": 0,
+        "totalNotification": 0
+         }
+    },
+    
+    {
+         "4:00 am": {
+        "instagram": 0,
+        "messenger": 0,
+        "snapchat": 0,
+        "email": 0,
+        "facebook": 0,
+        "totalNotification": 0
+         }
+    },
+    
+    {
+         "5:00 am": {
+        "instagram": 0,
+        "messenger": 0,
+        "snapchat": 0,
+        "email": 0,
+        "facebook": 0,
+        "totalNotification": 0
+         }
+    },
+    
+    {
+         "6:00 am": {
+        "instagram": 0,
+        "messenger": 0,
+        "snapchat": 0,
+        "email": 0,
+        "facebook": 0,
+        "totalNotification": 0
+         }
+    },
+    
+    {
+         "7:00 am": {
+        "instagram": 0,
+        "messenger": 0,
+        "snapchat": 0,
+        "email": 0,
+        "facebook": 0,
+        "totalNotification": 0
+         }
+    },
+    
+    {
+         "8:00 am": {
+        "instagram": 2,
+        "messenger": 2,
+        "snapchat": 0,
+        "email": 1,
+        "facebook": 0,
+        "totalNotification": 5
+         }
+    },
+    
+    {
+         "9:00 am": {
+        "instagram": 2,
+        "messenger": 0,
+        "snapchat": 0,
+        "email": 1,
+        "facebook": 0,
+        "totalNotification": 3
+         }
+    },
+    
+    {
+         "10:00 am": {
+        "instagram": 1,
+        "messenger": 2,
+        "snapchat": 3,
+        "email": 0,
+        "facebook": 2,
+        "totalNotification": 8
+         }
+    },
+    
+    {
+         "11:00 am": {
+        "instagram": 0,
+        "messenger": 2,
+        "snapchat": 7,
+        "email": 2,
+        "facebook": 0,
+        "totalNotification": 11
+         }
+    },
+    
+    {
+        "12:00 pm": {
+        "instagram": 0,
+        "messenger": 1,
+        "snapchat": 2,
+        "email": 0,
+        "facebook": 0,
+        "totalNotification": 3
+        }
+    },
+    
+    {
+         "1:00 pm": {
+        "instagram": 4,
+        "messenger": 0,
+        "snapchat": 0,
+        "email": 1,
+        "facebook": 0,
+        "totalNotification": 5
+         }
+    },
+    
+    {
+         "2:00 pm": {
+        "instagram": 2,
+        "messenger": 0,
+        "snapchat": 0,
+        "email": 0,
+        "facebook": 0,
+        "totalNotification": 2
+         }
+    },
+    
+    {
+         "3:00 pm": {
+        "instagram": 0,
+        "messenger": 2,
+        "snapchat": 4,
+        "email": 0,
+        "facebook": 0,
+        "totalNotification": 6
+         }
+    },
+    
+    {
+         "4:00 pm": {
+        "instagram": 5,
+        "messenger": 0,
+        "snapchat": 3,
+        "email": 0,
+        "facebook": 0,
+        "totalNotification": 8
+         }
+    },
+    
+    {
+         "5:00 pm": {
+        "instagram": 1,
+        "messenger": 0,
+        "snapchat": 4,
+        "email": 0,
+        "facebook": 0,
+        "totalNotification": 5
+         }
+    },
+    
+    {
+         "6:00 pm": {
+        "instagram": 0,
+        "messenger": 0,
+        "snapchat": 0,
+        "email": 0,
+        "facebook": 0,
+        "totalNotification": 0
+         }
+    },
+    
+    {
+         "7:00 pm": {
+        "instagram": 1,
+        "messenger": 11,
+        "snapchat": 0,
+        "email": 0,
+        "facebook": 0,
+        "totalNotification": 12
+         }
+    },
+    
+    {
+         "8:00 pm": {
+        "instagram": 0,
+        "messenger": 2,
+        "snapchat": 0,
+        "email": 0,
+        "facebook": 0,
+        "totalNotification": 2
+         }
+    },
+    
+    {
+         "9:00 pm":{
+        "instagram": 0,
+        "messenger": 1,
+        "snapchat": 0,
+        "email": 0,
+        "facebook": 0,
+        "totalNotification": 1
+         }
+    },
+    
+    {
+        "10:00 pm": {
+        "instagram": 7,
+        "messenger": 1,
+        "snapchat": 21,
+        "email": 0,
+        "facebook": 0,
+        "totalNotification": 29
+        }
+    },
+    
+    {
+         "11:00 pm": {
+        "instagram": 0,
+        "messenger": 0,
+        "snapchat": 3,
+        "email": 0,
+        "facebook": 0,
+        "totalNotification": 3
+         }
+    }
+  ]
---
+++ Week 9/WIP_DATA_3/WIP_DATA_3.js
@@ -0,0 +1,55 @@
+
+var data = {};
+var Sunday =[];
+var instagram, snapchat, messenger, facebook, email;
+var x=x;
+var y=y;
+var size = size;
+let time = (x, y);
+let number = size;
+
+function preload (){
+  data =loadJSON ("Sunday.js");
+}
+
+    
+function setup() {
+   createCanvas (windowWidth, windowHeight);
+   loadData ();
+   background (255);
+   noStroke ();
+   textSize (20);
+   size = number;
+   let instagram = color (200);
+   let messenger = color (30);
+   let snapchat = color (210);
+   let facebook = color (90);
+   let email = color (70); 
+   
+   // not sure if writing this correct?
+   let ellipse1 = (x,y,10, 10);
+     if (number(5, 10)) {}
+      
+     else 
+     if (number (10, 15)) {}
+   let ellipse2 = (x,y, 20, 20);
+      if (number (15, 30)) {}
+   let ellipse3 = (x,y,30,30);
+   
+      }
+     
+     
+function draw () {
+  //12:00 am data
+  
+  fill (messenger);
+  text (data.12:00 am.instagram, 210, 200);
+  
+  
+//for (var i=0; i<Sunday.length; i++) {
+  //Sunday [i].Sunday();
+  //Sunday [i].display ();
+  
+
+}
+  
